e3e0168a5b671d0d2caecec69eaadb061975a379
Yan Kaic <yankaic.as@gmail.com>
2025-09-11 20:38:19 -0300

    Leitura e adição de novas tarefas

--- Focolista/Task.swift
+++ Focolista/Task.swift
@@ -7,8 +7,13 @@
 import Foundation
 
 struct Task: Identifiable, Hashable {
-    let id: UUID = UUID()
-    var title: String
-    var isCompleted: Bool
-
+  let id: UUID
+  var title: String
+  var isCompleted: Bool
+  
+  init(id: UUID = UUID(), title: String, isCompleted: Bool) {
+    self.id = id
+    self.title = title
+    self.isCompleted = isCompleted
+  }  
 }
